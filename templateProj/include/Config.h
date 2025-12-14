f1d9ba8396a5204da27c57ca14dc9bc573c1b38e
indianProgrammer1 <153912889+indianProgrammer1@users.noreply.github.com>
2025-12-14 13:41:59 +0200

    Add default values to Config struct members
    Initialized struct members with default values using std::numeric_limits.

--- templateProj/include/Config.h
+++ templateProj/include/Config.h
@@ -2,11 +2,12 @@
 #include <string>
 
 struct Config {
-    double risk_target_pct;
-    double min_rr ;
-    int    max_slippage_bps ;
-    double max_vwap_dist ;
-    double threshold ; 
+    double risk_target_pct = std::numeric_limits<double>::quiet_NaN();
+    double min_rr = std::numeric_limits<double>::quiet_NaN();
+    int    max_slippage_bps = std::numeric_limits<int>::min();
+    double max_vwap_dist = std::numeric_limits<double>::quiet_NaN();
+    double threshold = std::numeric_limits<double>::quiet_NaN();
 };
 
 Config loadConfig(const std::string& path);
+
