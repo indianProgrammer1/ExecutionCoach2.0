2dd635c126a489b9a9fb01f12086d3cb444d2daf
indianProgrammer1 <153912889+indianProgrammer1@users.noreply.github.com>
2025-12-14 13:40:12 +0200

    Update Trade.h

--- templateProj/include/Trade.h
+++ templateProj/include/Trade.h
@@ -5,58 +5,39 @@ class Trade
 {
 private:
 	
-	int id;
+	int id{ 0 };
 	std::string side;
 	std::string Symbol;
-	const double risk;
-	int64_t entry;
-	int64_t exit;
-	int64_t stopLoss;
-	int64_t equity;
-	int64_t fee;
-	int64_t atr;
-	int slippageBps;
-	int qty;
-	double captureRatio;
-	double riskRewardReal;
-	double riskRewardPlan;
-	double vwapDistance;
-	std::chrono::nanoseconds ts_ns;
+	int64_t entry{ 0 };
+	int64_t exit{ 0 };
+	int64_t stopLoss{ 0 };
+	int64_t equity{ 0 };
+	int64_t fee{ 0 };
+	int64_t atr{ 0 };
+	double slippageBps{ 0 };
+	int qty{ 0 };
+	double captureRatio{ 0.0 };
+	double riskRewardReal{ 0.0 };
+	double riskRewardPlan{ 0.0 };
+	double vwapDistance{ 0.0 };
+	std::chrono::nanoseconds ts_ns{ 0 };
 
 public:
 
-	Trade() :
-		side(""),
-		Symbol(""),
-		id(),
-		risk(getEntry() - getStopLoss()),
-		entry(0.0),
-		exit(0.0),
-		stopLoss(0.0),
-		equity(0.0),
-		fee(0.0),
-		slippageBps(0),
-		qty(0),
-		captureRatio(0.0),
-		riskRewardReal(0.0),
-		riskRewardPlan(0.0),
-		vwapDistance(0.0),
-		atr(0.0),
-		ts_ns(std::chrono::nanoseconds(0))
-	 {};
+	Trade() = default;
+
 
 	std::string getSide() const { return side; }
 	void setSide(std::string val) { side = val; }
-	std::string GetSymbol() const { return Symbol; }
-	void SetSymbol(std::string val) { Symbol = val; }
-	const double getRisk() const { return risk; }
+	std::string getSymbol() const { return Symbol; }
+	void setSymbol(std::string val) { Symbol = val; }
 	std::chrono::nanoseconds Ts_ns() const { return ts_ns; }
 	void Ts_ns(std::chrono::nanoseconds val) { ts_ns = val; }
 	int getId() const { return id; }
 	void setId(int _id) { id = _id; }
 	int getQty() const { return qty; }
 	void setQty(int val) { qty = val; }
-	const int getSlippage() const { return slippageBps; }
+	const double getSlippage() const { return slippageBps; }
 	void setSlippage(double val) { slippageBps = val; }
 	double getVwapDistance() const { return vwapDistance; }
 	void setVwapDistance(double val) { vwapDistance = val; }
@@ -69,6 +50,9 @@ public:
 	
 
 	//from dollars to cents
+	int64_t getRisk() const {
+		return (std::llabs(getEntry() - getStopLoss()));
+	 }
 	int64_t getEquity() const { return equity; }
 	void setEquity(double val) { equity = static_cast<int64_t>(std::round(val * 100));}
 	
@@ -92,4 +76,6 @@ public:
 
 
 };
+
+
 
